ad46316fab4f73fd9db4ae2077fcb2d85bfc9fe7
jiangjieqim <jiangjieqim@hotmail.com>
2022-04-02 22:45:44 +0800

    发布ui

--- uipro/src/ui/layaUI.max.all.ts
+++ uipro/src/ui/layaUI.max.all.ts
@@ -2,13 +2,30 @@
 import View=laya.ui.View;
 import Dialog=laya.ui.Dialog;
 module ui {
-    export class HelpVIewUI extends View {
-		public showlist:Laya.List;
+    export class HelpItemVIewUI extends View {
+		public img:Laya.Image;
 
-        public static  uiView:any ={"type":"View","props":{"width":536,"height":104},"child":[{"type":"List","props":{"width":536,"var":"showlist","spaceX":4,"repeatX":5,"height":104,"hScrollBarSkin":" "}}]};
+        public static  uiView:any ={"type":"View","props":{"width":100,"height":100},"child":[{"type":"Image","props":{"width":100,"var":"img","height":100}}]};
         constructor(){ super()}
         createChildren():void {
         
+            super.createChildren();
+            this.createView(ui.HelpItemVIewUI.uiView);
+
+        }
+
+    }
+}
+
+module ui {
+    export class HelpVIewUI extends View {
+		public showlist:Laya.List;
+
+        public static  uiView:any ={"type":"View","props":{"width":536,"height":104},"child":[{"type":"List","props":{"width":536,"var":"showlist","spaceX":5,"repeatX":5,"height":104,"hScrollBarSkin":" "},"child":[{"type":"HelpItemVIew","props":{"renderType":"render","runtime":"ui.HelpItemVIewUI"}}]}]};
+        constructor(){ super()}
+        createChildren():void {
+        			View.regComponent("ui.HelpItemVIewUI",ui.HelpItemVIewUI);
+
             super.createChildren();
             this.createView(ui.HelpVIewUI.uiView);
 
