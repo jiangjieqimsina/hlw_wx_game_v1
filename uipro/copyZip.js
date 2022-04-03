d2b77421a4ee72e2c2da757f3ccb8703717da5f5
jiangjieqim <jiangjieqim@hotmail.com>
2022-04-03 23:16:31 +0800

    node copyZip.js
    //压缩copy外层目录的Code ABC到微信工程
    node copyZip.js zipCode

--- uipro/copyZip.js
+++ uipro/copyZip.js
@@ -1,19 +1,24 @@
 /*
-1.安装依赖npm install uglify-js -g
+1.安装依赖
+npm install uglify-js -g
+
+node copyZip.js
+
+//压缩copy外层目录的Code ABC到微信工程
+node copyZip.js zipCode
+
 */
 //压缩代码,copy代码到wxmini工程
 var fs=  require("fs");
-var process = require('child_process');
+var child_process = require('child_process');
 
-
+let time = new Date().getTime();
 
 let obj = JSON.parse(fs.readFileSync("module.json","utf-8"));
 // console.log(obj);
-
-process.execSync("layacmd compile");
-
+child_process.execSync("layacmd compile");
 //编译主域代码
-process.execSync("uglifyjs bin/js/main.js -m -o ../wxmini/main.js");
+child_process.execSync("uglifyjs bin/js/main.js -m -o ../wxmini/main.js");
 //编译子域代码
 let _root = "bin/js/";
 let ui = fs.readFileSync(_root+"ui/layaUI.max.all.js", "utf-8");
@@ -29,7 +34,16 @@ for(let i = 0;i < arr.length;i++){
 
 let sub =  fs.readFileSync(_root+"sub.js", "utf-8");
 fs.writeFileSync(_root+"subout.js",ui+rankui+sub,"utf-8");
-process.execSync("uglifyjs bin/js/subout.js -m -o ../wxmini/src/myOpenDataContext/subout.js");
+child_process.execSync("uglifyjs bin/js/subout.js -m -o ../wxmini/src/myOpenDataContext/subout.js");
 
 //压缩CodeC.js代码
-//process.execSync("uglifyjs ../CodeC.js -m -o ../wxmini/CodeC.js").toString();
+
+for(let i = 0;i <  process.argv.length ;i++){
+    if(process.argv[i]=="zipCode"){
+        child_process.execSync("uglifyjs ../CodeC.js -m -o ../wxmini/CodeC.js");
+    }
+}
+
+console.log(
+    `used ${(new Date().getTime() -time) / 1000} s`
+);
